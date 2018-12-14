741e7aaf450a468ff0733d9811be1985a8103393
Femi <llordbanks500@gmail.com>
2018-12-14 20:22:43 +0100

    Created script for SumOfMultiples excercise,  added its logic and linked the index.html to it

---
+++ exSumOfMultiples.js
@@ -0,0 +1,12 @@
+// Function t return the multiples of 3 & 5
+
+function sum(limit) {
+    let sumOf = 0;
+    for (let i = 0; i <= limit; i++) {
+        if ((i % 3 === 0) || (i % 5 === 0))
+            sumOf = sumOf + i;
+    }
+    console.log(sumOf);
+}
+
+sum(18);
