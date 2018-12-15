fc29d68b363a5e2d34d2c7ba4404c7a096064174
Femi <llordbanks500@gmail.com>
2018-12-15 03:11:57 +0100

    Simplified and modularized Grade calculator logic even further

--- exGrade.js
+++ exGrade.js
@@ -1,17 +1,22 @@
-function calculateGrade(marks) {
-    let noOfSubjects = marks.length;
-    let sum = 0;
-    for(let i = 0; i < noOfSubjects; i++)
-        sum += marks[i];
-    let avg = sum / noOfSubjects;
-    if (avg < 60) console.log('F');
-    if (avg >= 50 && avg < 60) console.log('E');
-    if (avg >= 60 && avg < 70) console.log('D');
-    if (avg >= 70 && avg < 80) console.log('C');
-    if (avg >= 80 && avg < 90) console.log('B');
-    if (avg >= 90 && avg < 100) console.log('A');
+function calculateGrade(marks) { 
+    let avg = calculateAverage(marks);
+    if (avg < 50) return 'F';
+    if (avg < 60) return 'E';
+    if (avg < 70) return 'D';
+    if (avg < 80) return 'C';
+    if (avg < 90) return 'B';
+    if (avg < 100) return 'A';
 }
 
-const marks = [80, 80, 50, 100, 100];
+function calculateAverage(array) {
+    let noOfSubjects = array.length;
+    let sum = 0;
+    for(let value of array)
+        sum += value;
+    let av = sum / noOfSubjects;
+    return av;
+}
 
-calculateGrade(marks);
+const marks = [80, 20, 50];
+
+console.log(calculateGrade(marks));
