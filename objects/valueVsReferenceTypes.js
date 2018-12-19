cdf8d09ca8c4ddfd1b33ff6b1ec464fd730c6d49
Femi <llordbanks500@gmail.com>
2018-12-19 22:03:44 +0100

    Added logic for valueVsReferenceTypes exercise

---
+++ objects/valueVsReferenceTypes.js
@@ -0,0 +1,38 @@
+let x = {value: 10};
+let y = x;
+
+x.value = 20;
+
+// Primitives are copied by their value
+// While objects OR REFERENCE TYPES are copied by their reference
+
+let number = 10;
+function increase(number) {
+    number++;
+    //console.log(number);
+}
+
+increase(number.value);
+console.log(number);
+
+// Value Types
+// Sumber
+// String
+// Boolean
+// Symbol
+// Undefined
+// Null
+
+let obj = {value: 10};
+function increase(obj) {
+    number++;
+    //console.log(number);
+}
+
+increase(obj.value);
+console.log(obj);
+
+// Reference types
+// Object
+// Function
+// Array
