f50285e474be614eebbb2f2e390340b97d9f624b
Femi <llordbanks500@gmail.com>
2018-12-20 18:57:13 +0100

    gb collection

---
+++ objects/garbageCollection.js
@@ -0,0 +1,4 @@
+let circle = { };
+console.log(circle);
+
+//  The garbage collector works in the background to find objects that are not being used and it then automatically de-allocates the memory location
