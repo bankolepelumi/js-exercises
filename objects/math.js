92e1cb0d53d515eaa9ab844bdbc5d6be79b09e34
Femi <llordbanks500@gmail.com>
2018-12-21 22:50:42 +0100

    Added logic for Math exercise

---
+++ objects/math.js
@@ -0,0 +1,10 @@
+
+for (let i = 0; i < 5; i++)
+console.log(Math.random());
+
+// Returns Base of natural logarithms
+function getNapier() {
+    console.log('Base of natural logarithms e:', Math.E);
+}
+getNapier();
+
